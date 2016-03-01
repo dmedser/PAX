9a552e94a2a94b569aaf9cc18539bb0d809e7964
dmedser <dmedser.mephi@gmail.com>
2016-03-01 19:46:32 +0300

    initial commit

---
+++ Receiver/main.c
@@ -0,0 +1,19 @@
+/*
+ * Receiver.c
+ *
+ * Created: 01.03.2016 19:18:21
+ * Author : dell
+ */ 
+
+#include <avr/io.h>
+
+#include "common.h"
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ Transmitter/main.c
@@ -0,0 +1,19 @@
+/*
+ * Transmitter.c
+ *
+ * Created: 01.03.2016 19:17:55
+ * Author : dell
+ */ 
+
+#include <avr/io.h>
+#include "common.h"
+
+int main(void)
+{
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
